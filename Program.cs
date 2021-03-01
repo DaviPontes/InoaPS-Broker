35016810d6d339f76f989494a315026369be476e
Davi <davihugomp@gmail.com>
2021-03-01 18:48:24 -0300

    Initial dotnet project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InoaPS_Broker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
